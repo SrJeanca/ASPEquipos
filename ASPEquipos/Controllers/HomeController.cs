6668e8bad96f3dcf1e93f406a2818c7b1e74ad08
SrJeanca <jeancasolano@hotmail.com>
2022-05-16 18:17:07 -0600

    Primera carga a GIt

---
+++ ASPEquipos/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPEquipos.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            //string servicio = "http://localhost:18677/api/Equipoes";
+            //var httpClient = new HttpClient();
+            //var json = await httpClient.GetStringAsync(servicio);
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ASPEquipos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPEquipos.Startup))]
+namespace ASPEquipos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
